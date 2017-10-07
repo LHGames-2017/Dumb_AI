902b06fc8bbf2cf799ebf5b424ae7cbdd5793bfd
Frédéric Fortin <f.fortin1997@gmail.com>
2017-10-07 17:41:47 -0400

    g

--- LHGames/Controllers/GameController.cs
+++ LHGames/Controllers/GameController.cs
@@ -103,7 +103,7 @@
             if(gameinfo.Player.CarriedResources <= 0.9f * gameinfo.Player.CarryingCapacity)
             {
                 cptDéplacement = 0;
-                List<Point> chemin = AI.TrouverChemin(gameinfo.Player.Position, ressources[0]-new Point(1,0), gameinfo.Player.HouseLocation);
+                List<Point> chemin = AI.TrouverChemin(gameinfo.Player.Position, ressources[0]-new Point(1,0), gameinfo.Player.HouseLocation, carte);
                 if (chemin.Count == 0)
                 {
                     ressources = ressources.OrderBy(x => Point.Distance(x, gameinfo.Player.Position)).ToList();
@@ -117,7 +117,7 @@
             else
             {
                 if (gameinfo.OtherPlayers.Count == 0 || gameinfo.OtherPlayers.Exists(x => Point.Distance(x.Value.Position, gameinfo.Player.Position) > 4))
-                    return AIHelper.CreateMoveAction(AI.TrouverChemin(gameinfo.Player.Position, gameinfo.Player.HouseLocation, gameinfo.Player.HouseLocation)[0]);
+                    return AIHelper.CreateMoveAction(AI.TrouverChemin(gameinfo.Player.Position, gameinfo.Player.HouseLocation, gameinfo.Player.HouseLocation, carte)[0]);
             }
             return AIHelper.CreateMoveAction(gameinfo.Player.Position);
         }

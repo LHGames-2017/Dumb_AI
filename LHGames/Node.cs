680c12a77b1ea26562dc3ee0d86465793bdc6c6b
Marilou Farmer <farmer.marilou@hotmail.fr>
2017-10-07 13:19:06 -0400

    Création Node

---
+++ LHGames/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarterProject.Web.Api
+{
+    public class Node
+    {
+        public Point Position { get; set; }
+        Node Parent { get; set; }
+        Point StartPoint { get; set; }
+        Point EndPoint { get; set; }
+        int gCost
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        int hCost
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        public int fCost
+        {
+            get
+            {
+                return gCost + hCost;
+            }
+        }
+        public Node(Point position, Point startPoint, Point endPoint, Node parent)
+        {
+            Position = position;
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+            Parent = parent;
+        }
+    }
+}
